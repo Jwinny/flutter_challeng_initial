a92c554e3ed48eff606ad87e16199f8d87eb6776
José Winny <jose.winny.dev@gmail.com>
2023-09-20 01:32:01 -0300

    Change background color

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.blueGrey,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
