3692d9d320180ac2ad1e40fa18442995cf5cf36e
José Winny <jose.winny.dev@gmail.com>
2023-09-20 01:23:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_challeng_initial
+ initial flutter aplication for experimentation
---
+++ android/app/src/main/kotlin/com/winny/diolab/flutter_challeng_initial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.winny.diolab.flutter_challeng_initial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
